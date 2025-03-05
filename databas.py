509017c8bdc33006d8b0e92a59618ae25b27f553
Windows 10 <adhambekhusinboyev@gmail.com>
2025-03-06 01:37:44 +0500

    Yangilanish, indi ishladi

--- databas.py
+++ databas.py
@@ -1,7 +1,9 @@
 from sqlite3 import *
 import sqlite3
+from pathlib import Path
 
-db = sqlite3.connect('/home/vakant2/vakant2/databasa/database.sqlite3')
+
+db = sqlite3.connect(str(Path(__file__).resolve().parent)+'databasa/database.sqlite3')
 sql = db.cursor()
 
 ###    bu baza bilan bog'lanish uchun ochilgan fayll
--- key.py
+++ key.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 
 
-TOKEN = "7855267108:AAFVmpmPaL58OF06mlnozB0YdHQDh-cs_r8"
+TOKEN = "6846010816:AAGy_vFZt4K-rw8RwUs5bXZUHiGsIuaV-Fc"
 
 bot = Bot(token=TOKEN, parse_mode='html')
 dp = Dispatcher(bot=bot, storage=storage)
