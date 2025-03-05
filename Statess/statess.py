f31d6fa865a058673e90d3cc245dfbb9c6425ab3
Adkhambek <114991635+khusinboev@users.noreply.github.com>
2025-03-05 10:51:13 +0500

    Add files via upload

---
+++ Statess/statess.py
@@ -0,0 +1,12 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+class From(StatesGroup):
+	channelAdd = State()
+	channelDelete = State()
+	send_msg = State()
+	forward_msg = State()
+	clear_msg = State()
+
+	sent_num = State()
+
+	one_S = State()
