ed4727352d76e6d086c99a3851c799082b408785
Windows 10 <adhambekhusinboyev@gmail.com>
2025-03-06 01:45:00 +0500

    Yangilanish, indi ishladi

--- databas.py
+++ databas.py
@@ -3,7 +3,7 @@ import sqlite3
 from pathlib import Path
 
 
-db = sqlite3.connect(str(Path(__file__).resolve().parent)+'databasa/database.sqlite3')
+db = sqlite3.connect(str(Path(__file__).resolve().parent)+'/databasa/database.sqlite3')
 sql = db.cursor()
 
 ###    bu baza bilan bog'lanish uchun ochilgan fayll
