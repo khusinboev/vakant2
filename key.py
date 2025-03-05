57a0ddabc49048de2dc8541eba8412a418e8555e
Windows 10 <adhambekhusinboyev@gmail.com>
2025-03-06 01:47:11 +0500

    Yangilanish, indi ishladi

--- key.py
+++ key.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 
 
-TOKEN = "6846010816:AAHjzDte6tr-lRSMJriM1OSMPG1wEiJxcqw"
+TOKEN = "6846010816:AAHquLwN6J0CKAu_O7HlFPs8Dm6lZIyG1B8"
 
 bot = Bot(token=TOKEN, parse_mode='html')
 dp = Dispatcher(bot=bot, storage=storage)
